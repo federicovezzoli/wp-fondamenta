b019f405fb18479813a078bfbe2b3af79d4c70ec
Federico Vezzoli <federico@dot-agency.it>
2014-01-23 10:52:32 +0100

    variuos fixes

--- base.php
+++ base.php
@@ -3,18 +3,11 @@
 
   <!--[if lt IE 7]><div class="alert"><?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?></div><![endif]-->
 
-  <?php
-    do_action('get_header');
-    // Use Bootstrap's navbar if enabled in config.php
-    if (current_theme_supports('bootstrap-top-navbar')) {
-      get_template_part('templates/header-top-navbar');
-    } else {
-      get_template_part('templates/header');
-    }
-  ?>
+  <?php do_action('get_header');
+  get_template_part('templates/header'); ?>
 
   <div class="wrap container" role="document">
-    <div class="content row">
+   
       <div class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
       </div><!-- /.main -->
@@ -23,7 +16,7 @@
         <?php include roots_sidebar_path(); ?>
       </aside><!-- /.sidebar -->
       <?php endif; ?>
-    </div><!-- /.content -->
+
   </div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
