1ad85a1f77a5059cb93aa506e285f7675dd0fbce
Federico Vezzoli <federico@dot-agency.it>
2014-02-28 17:19:09 +0100

    favicons and head settings

--- templates/head.php
+++ templates/head.php
@@ -7,8 +7,23 @@
   <meta charset="utf-8">
   <title><?php wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  
+  <!-- favicons -->
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicons/favicon.ico" />
+  <link rel=”apple-touch-icon” sizes=”114×114″ href=”<?php echo get_template_directory_uri(); ?>/assets/img/favicons/touch-icon-114.png” />
+  <link rel=”apple-touch-icon” sizes=”72×72″ href=”<?php echo get_template_directory_uri(); ?>/assets/img/favicons/touch-icon-72.png” />
+  <link rel=”apple-touch-icon” href=”<?php echo get_template_directory_uri(); ?>/assets/img/favicons/touch-icon-57.png” />
+  
   <?php wp_head(); ?>
+  
+  <!--[if lt IE 9]>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
+  <![endif]-->
+  
+  <!--[if lt IE 9]>
+  	<link href="<?php echo get_template_directory_uri(); ?>/assets/css/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <![endif]-->
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 </head>
