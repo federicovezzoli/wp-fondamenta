5bc382b22e9f0c027827a318087ade72cfd69010
Federico Vezzoli <federico@dot-agency.it>
2015-03-10 15:57:31 +0100

    fix

--- lib/scripts.php
+++ lib/scripts.php
@@ -47,7 +47,7 @@ function roots_jquery_local_fallback($src, $handle) {
   static $add_jquery_fallback = false;
 
   if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/vendor/jquery-1.10.2.min.js"><\/script>\')</script>' . "\n";
+    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/alone/jquery-1.10.2.min.js"><\/script>\')</script>' . "\n";
     $add_jquery_fallback = false;
   }
 
